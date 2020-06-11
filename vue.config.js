ce974b7aa1b908d5de976282e73c4765241f289e
songchunfeng <song4yu@163.com>
2020-06-11 23:28:24 +0800

    数据mock及svg图标引入

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const appDate = require('./data.json')
+const seller = appDate.seller
+const goods = appDate.goods
+const ratings = appDate.ratings
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -13,6 +18,28 @@ module.exports = {
     'cube-ui': {
       postCompile: true,
       theme: true
+    }
+  },
+  devServer: {
+    before (app) {
+      app.get('/api/seller', function (req, res) {
+        res.json({
+          error: 0,
+          data: seller
+        })
+      })
+      app.get('/api/goods', function (req, res) {
+        res.json({
+          error: 0,
+          data: goods
+        })
+      })
+      app.get('/api/ratings', function (req, res) {
+        res.json({
+          error: 0,
+          data: ratings
+        })
+      })
     }
   }
 }
